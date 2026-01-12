b406d3af0ff7375b3aac77fd4016b6757e34b3e7
srwoo <srwoo@hyundaiht.co.kr>
2026-01-12 14:29:44 +0900

    feat: 메뉴 위치 이동

--- front/src/routes/index.ts
+++ front/src/routes/index.ts
@@ -27,14 +27,6 @@ export const routeConfig: RouteMenuItem[] = [
     layout: ['footer'],
   },
   {
-    path: RouteLink.BOARD,
-    label: '자유게시판',
-    element: Board,
-    showInMenu: true,
-    authRequired: true,
-    layout: ['header', 'nav', 'footer'],
-  },
-  {
     path: RouteLink.NEWS,
     label: '공지사항',
     element: NewsFeedView,
@@ -47,6 +39,14 @@ export const routeConfig: RouteMenuItem[] = [
     element: NewsDetailView,
     showInMenu: false,
     authRequired: false,
+    layout: ['header', 'nav', 'footer'],
+  },
+  {
+    path: RouteLink.BOARD,
+    label: '자유게시판',
+    element: Board,
+    showInMenu: true,
+    authRequired: true,
     layout: ['header', 'nav', 'footer'],
   },
   {
@@ -70,7 +70,7 @@ export const routeConfig: RouteMenuItem[] = [
     path: RouteLink.SCHEDULE,
     label: '마이페이지',
     element: Schedule,
-    showInMenu: true,
+    showInMenu: false,
     authRequired: true,
     roles: [UserRole.USER, UserRole.ADMIN],
     layout: ['header', 'nav', 'footer'],
